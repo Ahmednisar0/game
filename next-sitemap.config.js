e771af7292c6862f09f153d2b7c3e63169842783
ahmedraza <aaza60232@gmail.com>
2025-05-26 15:41:19 +0500

    more

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://gamifox.com', // Replace with your real domain
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
